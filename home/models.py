541f2bb0f57eb76e3f3761577494a3dddc2e3a27
krijan_053 <krijan_053@icloud.com>
2023-02-16 23:21:29 +0545

    models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length = 300)
+    logo = models.CharField(max_length = 200)
+    slug = models.CharField(max_length = 300)
+
+    def __str__(self):
+        return self.name
+
+
+class SubCategory(models.Model):
+    name = models.CharField(max_length=300)
+    category = models.ForeignKey(on_delete = models.CASCADE)
+    slug = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
+
+
+class Slider(models.Model):
+    name = models.CharField(max_length=300)
+    image = models.ImageField(upload_to='media')
+    description = models.TextField()
+    url = models.URLField(max_length = 500,blank = True)
+
+    def __str__(self):
+        return self.name
+
+class Brand(models.Model):
+    name = models.CharField(max_length=300)
+    image = models.ImageFiled(upload_to = 'media')
+    slug = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
